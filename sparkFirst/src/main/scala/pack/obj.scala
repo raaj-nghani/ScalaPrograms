c7afcfad53cd6292da5a7e94159ed0d95c5b956e
Raj Kumar Bhardwaj <raaj.nghani@gmail.com>
2022-09-19 18:00:33 +0530

    final

--- sparkFirst/src/main/scala/pack/obj.scala
+++ sparkFirst/src/main/scala/pack/obj.scala
@@ -1,8 +1,10 @@
 package pack
 
 object obj {
+  def main(args:Array[String]):Unit={
   println("this is scala program")
   println("Enter name")
   println("firstname")
   println("----- hello -----")
+  }
 }
